34e4d242bab6098a6a5a7b9797eeccdce3a3de34
Michael Rudolph <michaelrudolph@posteo.net>
2021-12-14 14:31:39 +0100

    Make balance sheet tabs closeable

--- src/navigation/NavigationBar.tsx
+++ src/navigation/NavigationBar.tsx
@@ -31,29 +31,21 @@ interface IStyledTabs {
 
 const StyledTabs = styled(Tabs)<IStyledTabs>`
   & .Mui-selected {
-    background-color: ${(props) => props.$highlightColor || 'inherit'};
-    opacity: ${(props) => props.$highlightColor || 0.6};
+    background-color: ${(props) => props.$highlightColor};
+    opacity: ${(props) => props.$highlightColor};
   }
   & .MuiTabs-indicator {
     background-color: transparent;
   }
 `;
 
-enum ToolbarItems {
-  HOME = 'HOME',
-  BALANCE_SHEET_TABS = 'BALANCE_SHEET_TABS',
-}
-
 type NavigationProps = {
   user: User;
 };
 
 const NavigationBar = ({ user }: NavigationProps) => {
-  const [value, setValue] = useState<number>(0);
+  const [value, setValue] = useState<number | boolean>(false);
   const [sheets, setSheets] = useState<number[]>([]);
-  const [highlightedItem, setHighlightedItem] = useState<ToolbarItems>(
-    ToolbarItems.HOME
-  );
 
   const addSheet = async () => {
     const result = await axios.post(
@@ -68,19 +60,20 @@ const NavigationBar = ({ user }: NavigationProps) => {
       }
     );
     const { id } = result.data;
-    const sheetsLength = sheets.length;
     setSheets((sheets) => sheets.concat(id));
-    setValue(sheetsLength);
-    setHighlightedItem(ToolbarItems.BALANCE_SHEET_TABS);
+    setValue(id);
+  };
+
+  const deleteSheet = (idToDelete: number) => {
+    setSheets((sheets) => sheets.filter((id) => id !== idToDelete));
   };
 
   const goToHome = () => {
-    setHighlightedItem(ToolbarItems.HOME);
+    setValue(false);
   };
 
   const handleChange = (event: SyntheticEvent, newValue: number) => {
     setValue(newValue);
-    setHighlightedItem(ToolbarItems.BALANCE_SHEET_TABS);
   };
 
   const highlightColor = `rgba(0, 0, 0, 0.3)`;
@@ -90,9 +83,7 @@ const NavigationBar = ({ user }: NavigationProps) => {
       <AppBar key={'navigation'} position="static">
         <SmallToolbar>
           <SquaredIconButton
-            $backgroundColor={
-              highlightedItem === ToolbarItems.HOME ? highlightColor : undefined
-            }
+            $backgroundColor={value === false ? highlightColor : undefined}
             onClick={goToHome}
             size="large"
             edge="start"
@@ -113,18 +104,14 @@ const NavigationBar = ({ user }: NavigationProps) => {
             <FontAwesomeIcon icon={faPlus} />
           </SquaredIconButton>
           <StyledTabs
-            $highlightColor={
-              highlightedItem === ToolbarItems.BALANCE_SHEET_TABS
-                ? highlightColor
-                : undefined
-            }
+            $highlightColor={highlightColor}
             textColor="inherit"
             value={value}
             onChange={handleChange}
             aria-label="basic tabs example"
           >
             {sheets.map((sheet) => (
-              <BalanceSheetTab index={sheet} />
+              <BalanceSheetTab value={sheet} onDelete={deleteSheet} />
             ))}
           </StyledTabs>
         </SmallToolbar>
--- src/pages/BalanceSheetTab.tsx
+++ src/pages/BalanceSheetTab.tsx
@@ -2,23 +2,38 @@ import { IconButton, Tab, TabProps } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons/faTimes';
 import styled from 'styled-components';
+import { MouseEvent } from 'react';
 
 const StyledIconButton = styled(IconButton)`
   margin-left: 8px;
 `;
 
 interface BalanceSheetTabProps extends TabProps {
-  index: number;
+  onDelete: (value: number) => void;
 }
 
-const BalanceSheetTab = ({ index, ...tabProps }: BalanceSheetTabProps) => {
+const BalanceSheetTab = ({
+  onDelete,
+  value,
+  ...tabProps
+}: BalanceSheetTabProps) => {
+  const onClose = (e: MouseEvent) => {
+    e.stopPropagation();
+    onDelete(value);
+  };
+
   return (
     <Tab
       {...tabProps}
+      value={value}
       label={
         <span>
-          {`Balance sheet ${index}`}
-          <StyledIconButton size="small" color={'inherit'}>
+          {`Balance sheet ${value}`}
+          <StyledIconButton
+            onClick={(e) => onClose(e)}
+            size="small"
+            color={'inherit'}
+          >
             <FontAwesomeIcon icon={faTimes} />
           </StyledIconButton>
         </span>
