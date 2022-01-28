e0284aca959f8be8076fd6822481d71068b7f7c9
Michael Rudolph <michaelrudolph@posteo.net>
2022-01-28 16:13:07 +0100

    Disable caching for i18n language detector

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,13 +1,17 @@
 import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
 
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
 
-i18n.use(initReactI18next).use(LanguageDetector).use(Backend).init({
-  lng: 'en',
-  fallbackLng: 'en',
-  debug: true,
-});
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(Backend)
+  .init({
+    fallbackLng: 'en',
+    detection: { caches: [] },
+    debug: true,
+  });
 
 export default i18n;
