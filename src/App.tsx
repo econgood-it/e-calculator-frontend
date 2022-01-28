0e96688aa9ddc134eaf6d6eeedd9ac9c328e7109
Michael Rudolph <michaelrudolph@posteo.net>
2022-01-28 17:43:55 +0100

    Add promise rejection on error

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,21 @@ import { ThemeProvider } from 'styled-components';
 import axios from 'axios';
 
 axios.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    console.log(error.response.status);
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  function (error) {
     if (error.response.status === 401) {
       window.location.pathname = '/login';
     }
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
   }
 );
+
 export const themeOptions: ThemeOptions = {
   palette: {
     primary: {
