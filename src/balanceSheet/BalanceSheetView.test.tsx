1c906270484d367f24b3cbf75049e6e7708260c6
Michael Rudolph <michaelrudolph@posteo.net>
2022-02-11 17:06:47 +0100

    Add a test for the supplier ratings

--- src/balanceSheet/BalanceSheetView.test.tsx
+++ src/balanceSheet/BalanceSheetView.test.tsx
@@ -1,20 +1,16 @@
 import '@testing-library/jest-dom';
-import { screen, waitFor } from '@testing-library/react';
+import { fireEvent, screen, waitFor } from '@testing-library/react';
 import BalanceSheetView from './BalanceSheetView';
 import axios from 'axios';
 import { renderWithTheme } from '../testUtils/rendering';
 import { ratingMock } from '../testUtils/balanceSheets';
+import { user } from '../testUtils/user';
 
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('BalanceSheetView', () => {
-  it('renders', async () => {
-    const user = {
-      token: 'fjdlksajrklejwlqjfkljkl',
-      expires: '2022-02-18T14:24:37+01:00',
-      user: 3,
-    };
+  it('renders text Company Facts', async () => {
     mockedAxios.get.mockResolvedValueOnce({
       data: {
         rating: ratingMock,
@@ -23,5 +19,17 @@ describe('BalanceSheetView', () => {
     renderWithTheme(<BalanceSheetView balanceSheetId={1} user={user} />);
     const el = await waitFor(() => screen.getAllByText('Company Facts'));
     expect(el).toHaveLength(2);
+  });
+
+  it('renders suppliers ratings', async () => {
+    mockedAxios.get.mockResolvedValueOnce({
+      data: {
+        rating: ratingMock,
+      },
+    });
+    renderWithTheme(<BalanceSheetView balanceSheetId={1} user={user} />);
+    const suppliersNavItem = await waitFor(() => screen.getByText('Suppliers'));
+    fireEvent.click(suppliersNavItem);
+    expect(screen.getByText('A1.1')).toBeInTheDocument();
   });
 });
