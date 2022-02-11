492f488b58bc7c55c93829994c7c526d33eb5380
Michael Rudolph <michaelrudolph@posteo.net>
2022-02-11 16:57:14 +0100

    Create rendering method for tests

--- src/balanceSheet/BalanceSheetView.test.tsx
+++ src/balanceSheet/BalanceSheetView.test.tsx
@@ -1,17 +1,36 @@
 import '@testing-library/jest-dom';
-import { render, screen, waitFor } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import BalanceSheetView from './BalanceSheetView';
 import axios from 'axios';
-import { createTheme, ThemeProvider as MuiThemeProvider } from '@mui/material';
-import { ThemeProvider } from 'styled-components';
-import { themeOptions } from '../App';
+import { renderWithTheme } from '../testUtils/rendering';
 
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
-const theme = createTheme(themeOptions);
-
 describe('BalanceSheetView', () => {
+  const ratingMock = {
+    topics: [
+      {
+        shortName: 'A1',
+        name: 'Menschenwürde in der Zulieferkette',
+        estimations: 0,
+        aspects: [
+          {
+            shortName: 'A1.1',
+            name: 'Arbeitsbedingungen und gesellschaftliche Auswirkungen in der Zulieferkette',
+            estimations: 0,
+            isPositive: true,
+          },
+          {
+            shortName: 'A1.2',
+            name: 'Negativ-Aspekt: Verletzung der Menschenwürde in der Zulieferkette',
+            estimations: 0,
+            isPositive: false,
+          },
+        ],
+      },
+    ],
+  };
   it('renders', async () => {
     const user = {
       token: 'fjdlksajrklejwlqjfkljkl',
@@ -20,39 +39,10 @@ describe('BalanceSheetView', () => {
     };
     mockedAxios.get.mockResolvedValueOnce({
       data: {
-        rating: {
-          topics: [
-            {
-              shortName: 'A1',
-              name: 'Menschenwürde in der Zulieferkette',
-              estimations: 0,
-              aspects: [
-                {
-                  shortName: 'A1.1',
-                  name: 'Arbeitsbedingungen und gesellschaftliche Auswirkungen in der Zulieferkette',
-                  estimations: 0,
-                  isPositive: true,
-                },
-                {
-                  shortName: 'A1.2',
-                  name: 'Negativ-Aspekt: Verletzung der Menschenwürde in der Zulieferkette',
-                  estimations: 0,
-                  isPositive: false,
-                },
-              ],
-            },
-          ],
-        },
+        rating: ratingMock,
       },
     });
-    render(
-      <MuiThemeProvider theme={theme}>
-        <ThemeProvider theme={theme}>
-          <BalanceSheetView balanceSheetId={1} user={user} />
-        </ThemeProvider>
-      </MuiThemeProvider>
-    );
-    // screen.debug();
+    renderWithTheme(<BalanceSheetView balanceSheetId={1} user={user} />);
     const el = await waitFor(() => screen.getAllByText('Company Facts'));
     expect(el).toHaveLength(2);
   });
