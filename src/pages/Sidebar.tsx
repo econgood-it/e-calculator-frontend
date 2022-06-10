0401bb79edabc3278a0be38e1d83e80b00679141
Michael Rudolph <michaelrudolph@posteo.net>
2022-06-10 17:12:34 +0200

    Simplify Sidebar

--- src/pages/Sidebar.tsx
+++ src/pages/Sidebar.tsx
@@ -1,147 +1,85 @@
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
-import CssBaseline from '@mui/material/CssBaseline';
 import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faBars,
-  faChevronLeft,
-  faFolderOpen,
-} from '@fortawesome/free-solid-svg-icons';
-import { Outlet } from 'react-router-dom';
+import { faBars, faFolderOpen } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
-import { ListSubheader } from '@mui/material';
+import { AppBar, Box, ListSubheader } from '@mui/material';
 import { Trans, useTranslation } from 'react-i18next';
+import styled from 'styled-components';
+import { Outlet } from 'react-router-dom';
 
-const drawerWidth = 240;
+const FixedAppBar = styled(AppBar)`
+  z-index: ${(props) => props.theme.zIndex.drawer + 1};
+`;
 
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
-  open?: boolean;
-}>(({ theme, open }) => ({
-  flexGrow: 1,
-  padding: theme.spacing(3),
-  transition: theme.transitions.create('margin', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  marginLeft: `-${drawerWidth}px`,
-  ...(open && {
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  }),
-}));
+const Content = styled.div<{ $open: boolean; $drawerWidth: number }>`
+  margin-top: 16px;
+  margin-left: ${(props) => (props.$open ? props.$drawerWidth : 16)}px;
+`;
 
-interface AppBarProps extends MuiAppBarProps {
-  open?: boolean;
-}
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})<AppBarProps>(({ theme, open }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: `${drawerWidth}px`,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-  justifyContent: 'flex-end',
-}));
+const DrawerWithFixedWidth = styled(Drawer)<{ $drawerWidth: number }>`
+  &.MuiDrawer-paper {
+    width: ${(props) => props.$drawerWidth}px;
+  }
+`;
 
 export default function Sidebar() {
-  const [open, setOpen] = useState<boolean>(true);
+  const [open, setOpen] = useState<boolean>(false);
   const { t } = useTranslation('sidebar');
 
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
+  const drawerWidth = 240;
+  const toogleSidebar = () => {
+    setOpen(!open);
   };
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar position="fixed" open={open}>
+    <>
+      <FixedAppBar>
         <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open sidebar"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={{ mr: 2, ...(open && { display: 'none' }) }}
-          >
+          <IconButton aria-label="toogle sidebar" onClick={toogleSidebar}>
             <FontAwesomeIcon icon={faBars} />
           </IconButton>
           <Typography variant="h6" noWrap component="div">
             Persistent drawer
           </Typography>
         </Toolbar>
-      </AppBar>
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
+      </FixedAppBar>
+      <DrawerWithFixedWidth
         variant="persistent"
         anchor="left"
         open={open}
+        $drawerWidth={drawerWidth}
       >
-        <DrawerHeader>
-          <IconButton aria-label={'close sidebar'} onClick={handleDrawerClose}>
-            <FontAwesomeIcon icon={faChevronLeft} />
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <ListSubheader>
-          <Trans t={t}>Overview</Trans>
-        </ListSubheader>
-        <List>
-          <ListItem key={'balancesheets'} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <FontAwesomeIcon icon={faFolderOpen} />
-              </ListItemIcon>
-              <ListItemText primary={<Trans t={t}>Balance sheets</Trans>} />
-            </ListItemButton>
-          </ListItem>
-        </List>
-      </Drawer>
-      <Main open={open}>
-        <DrawerHeader />
-        <Outlet />
-      </Main>
-    </Box>
+        <Toolbar />
+        <Box>
+          <ListSubheader>
+            <Trans t={t}>Overview</Trans>
+          </ListSubheader>
+          <List>
+            <ListItem key={'balancesheets'} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <FontAwesomeIcon icon={faFolderOpen} />
+                </ListItemIcon>
+                <ListItemText primary={<Trans t={t}>Balance sheets</Trans>} />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </Box>
+      </DrawerWithFixedWidth>
+      <Box>
+        <Toolbar />
+        <Content $open={open} $drawerWidth={drawerWidth}>
+          <Outlet />
+        </Content>
+      </Box>
+    </>
   );
 }
