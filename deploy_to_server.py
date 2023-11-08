b2e477ee2fbdcec2834e2ac072862fe51bc8eecc
Michael Rudolph <michaelrudolph@posteo.net>
2023-11-08 17:53:57 +0100

    Adapt deploy script for docker deployment

--- deploy_to_server.py
+++ deploy_to_server.py
@@ -7,6 +7,8 @@ import git
 
 yarn = 'yarn'
 run = 'run'
+docker = 'docker'
+compose = 'compose'
 
 
 def has_unpushed_changes():
@@ -56,15 +58,20 @@ def run_tests():
     subprocess.run([yarn, run, 'test'], env=my_env, check=True)
 
 
-def compile_typescript_to_javascript(backend_url: str):
-    my_env = os.environ.copy()
-    my_env["REACT_APP_BACKEND_DOMAIN"] = backend_url
-    my_env["GENERATE_SOURCEMAP"] = 'false'
-    subprocess.run([yarn, 'build'], env=my_env, check=True)
-
-
-def rsync(folder: str, server_domain: str, server_folder: str):
-    subprocess.run(['rsync', '-av', folder, f"{server_domain}:{server_folder}", '--delete'], check=True)
+def build_and_deploy_remotely(server_domain: str, backend_url: str):
+    commands = [
+        'cd /var/docker/e-calculator-frontend',
+        'git pull',
+        "rm -r node_modules"
+        f"{yarn} install"
+        "rm -r build"
+        f"REACT_APP_BACKEND_DOMAIN={backend_url} GENERATE_SOURCEMAP=false yarn build"
+        f"mv ./build ./volumes/www"
+        f"{docker} {compose} down",
+        f"{docker} {compose} up"
+    ]
+    full_command = " && ".join(commands)
+    subprocess.run(['ssh', server_domain, full_command], check=True)
 
 
 def rm_folder(folder: str):
@@ -82,13 +89,10 @@ def main(args):
     check_linting()
     logging.info(f"Run tests")
     run_tests()
-    logging.info(f"Build and compile")
-    rm_folder(folder='build')
-    compile_typescript_to_javascript(backend_url='https://calculator.test.ecogood.org')
-    server_domain = 'ecg04-bcalcweb_test@ecg04.hostsharing.net'
-    server_folder = 'doms/ecalc.test.ecogood.org/htdocs-ssl'
-    logging.info(f"Copy build folder to {server_domain}")
-    rsync(folder='build/', server_domain=server_domain, server_folder=server_folder)
+    server_domain = 'ecg@prod.econgood.org' if args.environment == 'prod' else 'ecg@dev.econgood.org'
+    backend_url = 'https://balance-sheet-api.prod.econgood.org/' if args.environment == 'prod' else 'https://balance-sheet-api.dev.econgood.org/'
+    logging.info(f"Build and deploy to {server_domain} and using backend api {backend_url}")
+    build_and_deploy_remotely(server_domain=server_domain, backend_url=backend_url)
     logging.info(f"Deployment finished")
 
 
